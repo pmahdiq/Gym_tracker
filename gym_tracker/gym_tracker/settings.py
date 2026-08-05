43dd4870e7b806958b694b9cf3f919bbae8d136b
Mahdi Esmailzade <m.esmailzade1377@gmail.com>
2026-08-05 19:20:46 +0330

    learned how django workflow works

--- gym_tracker/gym_tracker/settings.py
+++ gym_tracker/gym_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'template_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'gym_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
