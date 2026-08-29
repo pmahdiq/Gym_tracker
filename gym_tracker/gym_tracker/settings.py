0f09271b7c77d1c3b8f9a689ba728fb15b803768
Mahdi Esmailzade <m.esmailzade1377@gmail.com>
2026-08-29 18:21:17 +0330

    this deployment journey will kill me

--- gym_tracker/gym_tracker/settings.py
+++ gym_tracker/gym_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,9 +33,15 @@ ALLOWED_HOSTS = [
     os.environ.get("RENDER_EXTERNAL_HOSTNAME", ""),
     os.environ.get("PYTHONANYWHERE_HOSTNAME", ""),
     ".pythonanywhere.com",
+    os.environ.get("RAILWAY_PUBLIC_DOMAIN", ""),
+    ".up.railway.app",
 ]
 
 SITE_ID = 1
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN', '')}",
+]
 
 # Application definition
 
@@ -86,10 +93,10 @@ WSGI_APPLICATION = 'gym_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
